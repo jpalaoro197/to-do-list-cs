2bc700b40a36be2ba738e7452d49a49fc989f7ac
jacobpalaoro <jpalaoro197@gmail.com>
2022-07-12 14:47:01 -0700

    add a spot for all instances to belong, and add static list to return the instances.

--- List/Models/Item.cs
+++ List/Models/Item.cs
@@ -7,8 +7,13 @@ namespace ToDoList.Models
     public string Description { get; set; }
     private static List<Item> _instances = new List<Item> {};
     public Item(string description)
+    public static List<Item> GetAll()
+    {
+      return _instances;
+    }
     {
       Description = description;
+      _instances.Add(this);
     }
 
   }
