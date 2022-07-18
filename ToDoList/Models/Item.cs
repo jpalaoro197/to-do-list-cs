6d1c20642a97029b4e78a10dfaaaa09a5d69df77
jacobpalaoro <jpalaoro197@gmail.com>
2022-07-18 15:11:26 -0700

    fix bugs and rename some files

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -6,16 +6,18 @@ namespace ToDoList.Models
   {
     public string Description { get; set; }
     private static List<Item> _instances = new List<Item> {};
-    public Item(string description)
+
+    public Item (string description)
     {
       Description = description;
       _instances.Add(this);
     }
 
-     public static List<Item> GetAll()
+    public static List<Item> GetAll()
     {
       return _instances;
     }
+
     public static void ClearAll()
     {
       _instances.Clear();
