222cde31cc35e6960f0d4c62b422622af0b5a28d
jacobpalaoro <jpalaoro197@gmail.com>
2022-07-18 14:18:29 -0700

    replace program.cs file code with proper boilerplate code.

--- List/Program.cs
+++ List/Program.cs
@@ -1,30 +1,20 @@
-using System.Collections.Generic;
-using System;
-using ToDoList.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
-namespace Program.ToDoList
+namespace ToDoList
 {
   public class Program
   {
-    public static Dictionary<string
-    public static void Main()
+    public static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to the To Do List, Would you like to add an item to your list or view your list? (Add/View)");
-      string response = Console.ReadLine();
-      if (response == "Add" || response == "add")
-      {
-        Console.WriteLine("What would you like to add?");
-      } else if (response == "View" || response == "view") {
-        Console.WriteLine("Here's your current to do list");
-      } else {
-        Console.WriteLine("Please use 'Add' or 'View'");
-      }
-      string userTask = Console.ReadLine();
-      Item userItem = new Item(userTask);
-      List<Item> newList = new List<Item> {};
-      List<Item> result = Item.GetAll();
-      Console.WriteLine(result);
-      Main();
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
+
+      host.Run();
     }
   }
 }
