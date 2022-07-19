d1e1227adde7e4c6e1b95411bc81dba6631c0d5b
jacobpalaoro <jpalaoro197@gmail.com>
2022-07-19 10:22:06 -0700

    add an id property that assigns each item a new unique id
    Co-authored-by: DonovanWeber <donovanweber03@gmail.com>

--- List.Tests/ModelTests/ItemTests.cs
+++ List.Tests/ModelTests/ItemTests.cs
@@ -72,6 +72,20 @@ namespace ToDoList.Tests
       //Assert
       CollectionAssert.AreEqual(newList, result);
     }
+      [TestMethod]
+      public void GetId_ItemsInstantiateWithAnIdAndGetterReturns_Int()
+      {
+       //Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+
+       //Act
+      int result = newItem.Id;
+
+        //Assert
+      Assert.AreEqual(1, result);
+      }
+    [TestMethod]
 
   }
 }
