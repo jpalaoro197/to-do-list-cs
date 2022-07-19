86cc264a6be903436e5281dd7d82c82afbb30389
jacobpalaoro <jpalaoro197@gmail.com>
2022-07-19 13:40:17 -0700

    add finding item by id, and get details, and also fix the delete function.
    Co-authored-by: DonovanWeber <donovanweber03@gmail.com>

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -24,6 +24,13 @@ namespace ToDoList.Models
     {
       _instances.Clear();
     }
+    
+    public static Item Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+
+
 
   }
 }
