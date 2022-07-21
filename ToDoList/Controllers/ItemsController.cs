e5389eb83f42abd7bb625949ee8ec3d59edf5c5b
jacobpalaoro <jpalaoro197@gmail.com>
2022-07-21 08:59:26 -0700

    fully functioning project, add categories and items within them categories.

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -6,6 +6,17 @@ namespace ToDoList.Controllers
   public class ItemsController : Controller
   {
 
+    [HttpGet("/categories/{categoryId}/items/{itemId}")]
+    public ActionResult Show(int categoryId, int itemId)
+    {
+      Item item = Item.Find(itemId);
+      Category category = Category.Find(categoryId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("item", item);
+      model.Add("category", category);
+      return View(model);
+    }
+
     [HttpGet("/items")]
     public ActionResult Index()
     {
@@ -13,18 +24,13 @@ namespace ToDoList.Controllers
       return View(allItems);
     }
 
-    [HttpGet("/items/new")]
-    public ActionResult New()
+    [HttpGet("/categories/{categoryId}/items/new")]
+    public ActionResult New(int categoryId)
     {
-      return View();
+      Category category = Category.Find(categoryId);
+      return View(category);
     }
 
-    [HttpPost("/items")]
-    public ActionResult Create(string description)
-    {
-      Item myItem = new Item(description);
-      return RedirectToAction("Index");//instead of View("Index")
-    }
     
     [HttpPost("/items/delete")]
     public ActionResult DeleteAll()
