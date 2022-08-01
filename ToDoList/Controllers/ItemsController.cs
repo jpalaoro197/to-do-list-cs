75921197bda649f885914e8b456089ae1c464027
jacobpalaoro <jpalaoro197@gmail.com>
2022-08-01 11:35:55 -0700

    add edit function back to items, manystyleee

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -21,19 +21,24 @@ namespace ToDoList.Controllers
       return View(_db.Items.ToList());
     }
 
-//     public ActionResult Create()
-//     {
-//       ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
-//       return View();
-//     }
+    public ActionResult Create()
+    {
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+      return View();
+    }
 
-//     [HttpPost]
-//     public ActionResult Create(Item item)
-//     {
-//       _db.Items.Add(item);
-//       _db.SaveChanges();
-//       return RedirectToAction("Index");
-//     }
+    [HttpPost]
+    public ActionResult Create(Item item, int CategoryId)
+    {
+       _db.Items.Add(item);
+       _db.SaveChanges();
+       if (CategoryId != 0)
+        {
+          _db.CategoryItem.Add(new CategoryItem() { CategoryId = CategoryId, ItemId = item.ItemId });
+          _db.SaveChanges();
+        }
+      return RedirectToAction("Index");
+    }
 
     public ActionResult Details(int id)
     {
@@ -44,20 +49,23 @@ namespace ToDoList.Controllers
       return View(thisItem);
     }
 
-//     public ActionResult Edit(int id)
-//     {
-//       var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
-//       ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
-//       return View(thisItem);
-//     }
+    public ActionResult Edit(int id)
+    {
+      var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+      return View(thisItem);
+    }
 
-//     [HttpPost]
-//     public ActionResult Edit(Item item)
-//     {
-//       _db.Entry(item).State = EntityState.Modified;
-//       _db.SaveChanges();
-//       return RedirectToAction("Index");
-//     }
+     [HttpPost]
+    public ActionResult Edit(Item item, int CategoryId)
+    {
+      if (CategoryId != 0)
+      {
+        _db.CategoryItem.Add(new CategoryItem() { CategoryId = CategoryId, ItemId = item.ItemId });
+      }
+      _db.Entry(item).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
 
 //     public ActionResult Delete(int id)
 //     {
